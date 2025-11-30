d3ea0989d8288cd6c91d8181d5ed777f3583baba
naz.sag <cgulaz2@gmail.com>
2025-11-30 11:33:03 +0300

    Fix frontend: import os

--- frontend/app.py
+++ frontend/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
+import os
 
 # ==================================================================================
 # НАСТРОЙКА СТРАНИЦЫ
